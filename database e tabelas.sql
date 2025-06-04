7d450382ddb565e3183460354eff779df8d32c50
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-04 09:22:05 -0300

    Utilizando chave primaria e auto incremento na tabela

--- database e tabelas.sql
+++ database e tabelas.sql
@@ -2,7 +2,7 @@ CREATE DATABASE db_curso_web;
 DROP DATABASE db_curso_web;
 
 create table tb_cursos (
-	id_curso int not null,
+	id_curso int auto_increment primary key,
     imagem_curso varchar(100) not null,
     nome_curso char(50) not null,
     resumo text null,
