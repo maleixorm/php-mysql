a9a6fe5e678e08afe8d1f41e82c3a6b6e2372a87
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-05 02:28:50 -0300

    Trabalhando com inserts na tabela

---
+++ insert and select.sql
@@ -0,0 +1,7 @@
+insert into tb_cursos (imagem_curso, nome_curso, resumo, data_cadastro, ativo, investimento, carga_horaria) 
+values ('curso_angular.jpg','Web Completo com JS,TS e Angular','Aprenda a criar aplicações front-end incríveis com Javascript, Typescript e Angular.','2018-07-17 12:25:35',1,575.86,35);
+
+insert into tb_cursos (imagem_curso, nome_curso, resumo, data_cadastro, ativo, investimento, carga_horaria)
+values ('curso_node.jpg','Curso Completo NodeJS e MongoDB','Crie aplicações back-end incríveis com a plataforma NodeJS e o Banco de dados MongoDB.','2018-07-17 12:38:7',1,550.75,15);
+
+select * from tb_cursos;
