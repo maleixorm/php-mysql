1b3774a50f4d3c90c127f7eedbfe9cf83c96ddc7
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-06 16:35:06 -0300

    Populando a tb_imagens

--- populando store.sql
+++ populando store.sql
@@ -4,6 +4,10 @@ INSERT INTO tb_produtos(produto, valor) VALUES ('Smartphone LG K10 Dual Chip And
 INSERT INTO tb_descricoes_tecnicas(id_produto, descricao_tecnica) VALUES (1, 'O novo Inspiron Dell oferece um design elegante e tela infinita que amplia seus sentidos, mantendo a sofisticação e medidas compactas...');
 INSERT INTO tb_descricoes_tecnicas(id_produto, descricao_tecnica) VALUES (2, 'A smart TV da Samsung possui tela de 40" e oferece resolução Full HD, imagens duas vezes melhores que TVs HDs padrão...');
 INSERT INTO tb_descricoes_tecnicas(id_produto, descricao_tecnica) VALUES (3, 'Saia da mesmice. O smartphone LG está mais divertido, rápido, fácil, cheio de selfies e com tela HD de incríveis 5,3"...');
+INSERT INTO tb_imagens(id_produto, url_imagem) VALUES (1, 'notebook_1.jpg'), (1, 'notebook_2.jpg'), (1, 'notebook_3.jpg');
+INSERT INTO tb_imagens(id_produto, url_imagem) VALUES (2, 'smarttv_1.jpg'), (2, 'smarttv_2.jpg');
+INSERT INTO tb_imagens(id_produto, url_imagem) VALUES (3, 'smartphone_1.jpg');
 
 select * from tb_produtos;
 select * from tb_descricoes_tecnicas;
+select * from tb_imagens;
