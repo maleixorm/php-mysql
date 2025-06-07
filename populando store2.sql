2599e81561a5a5d0bf9c38c33bedcbca14fef207
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-07 18:40:55 -0300

    Alterando dados e consultando tabelas

--- populando store2.sql
+++ populando store2.sql
@@ -1,4 +1,4 @@
-INSERT INTO tb_clientes(nome, idade) VALUES('Jorge', 29);	
+INSERT INTO tb_clientes(nome, idade) VALUES('Jorge', 29);
 INSERT INTO tb_pedidos(id_cliente) VALUES(1);	
 INSERT INTO tb_pedidos_produtos(id_pedido, id_produto) VALUES(1, 2);
 INSERT INTO tb_pedidos_produtos(id_pedido, id_produto) VALUES(1, 3);
@@ -7,3 +7,7 @@ INSERT INTO tb_pedidos_produtos(id_pedido, id_produto) VALUES(2,3);
 INSERT INTO tb_clientes(nome, idade) VALUES('Jamilton', 30);
 INSERT INTO tb_pedidos(id_cliente) VALUES(2);
 INSERT INTO tb_pedidos_produtos(id_pedido, id_produto) VALUES(3,1);
+
+select * from tb_clientes;
+select * from tb_pedidos;
+select * from tb_pedidos_produtos;
