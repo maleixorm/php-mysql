9f69fadb14cb989821699ed10051f49c3776cc84
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-12 17:03:13 -0300

    Tratando exceptions no pdo

--- index.php
+++ index.php
@@ -3,4 +3,9 @@ $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
 $user = 'php';
 $pass = 123456;
 
-$conexao = new PDO($dsn, $user, $pass);
+try {
+    $conexao = new PDO($dsn, $user, $pass);
+} catch (PDOException $e) {
+    echo 'Erro: ' . $e->getCode() . ' - Mensagem: ' . $e->getMessage(); 
+}
+
