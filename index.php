210ac023b31d685e560c65516c3c3c12c2e89cfa
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-12 17:55:29 -0300

    Utilizando o select e o fetchAll para armazenar os dados do banco em um statement

--- index.php
+++ index.php
@@ -5,20 +5,12 @@ $pass = 123456;
 
 try {
     $conexao = new PDO($dsn, $user, $pass);
-    
-    $query = "CREATE TABLE IF NOT EXISTS tb_usuarios(
-        id int not null primary key auto_increment,
-        nome varchar(50) not null,
-        email varchar(100) not null,
-        senha varchar(32) not null
-    );";
-
-    $query2 = "INSERT INTO tb_usuarios(nome, email, senha)
-        values ('John Connor', 'john@skynet.com', '123456');
-    ";
-
-    $conexao->exec($query2);
+    $query = 'Select * from tb_usuarios;';
+    $stmt = $conexao->query($query);
+    $listagem = $stmt->fetchAll();
+    echo '<pre>';
+    print_r($listagem);
+    echo '</pre>';
 } catch (PDOException $e) {
     echo 'Erro: ' . $e->getCode() . ' - Mensagem: ' . $e->getMessage(); 
 }
-
