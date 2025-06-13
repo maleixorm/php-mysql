81bad090b89de7659414c3341973b3a615f03506
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-13 10:09:13 -0300

    Retornando um registro único

--- index.php
+++ index.php
@@ -5,12 +5,13 @@ $pass = 123456;
 
 try {
     $conexao = new PDO($dsn, $user, $pass);
-    $query = 'Select * from tb_usuarios;';
+    $query = "SELECT * FROM tb_usuarios ORDER BY nome DESC LIMIT 1;";
     $stmt = $conexao->query($query);
-    $listagem = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $listagem = $stmt->fetch(PDO::FETCH_OBJ);
     echo '<pre>';
     print_r($listagem);
     echo '</pre>';
+    echo $listagem->nome;
 } catch (PDOException $e) {
     echo 'Erro: ' . $e->getCode() . ' - Mensagem: ' . $e->getMessage(); 
 }
