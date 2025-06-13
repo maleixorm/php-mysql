16d2ff9ab6bf62c43411086bdf9bd685eda5ca93
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-13 10:09:33 -0300

    Selecionando dados

--- pdo.sql
+++ pdo.sql
@@ -1,14 +1,17 @@
-create table if not exists tb_usuarios(
+CREATE TABLE IF NOT EXISTS tb_usuarios(
     id int not null primary key auto_increment,
     nome varchar(50) not null,
     email varchar(100) not null,
     senha varchar(32) not null
 );
 
-insert into 
+INSERT INTO 
     tb_usuarios(nome, email, senha)
-values 
+VALUES 
     ('Kyle Reese', 'kyle@skynet.com', '123456'), 
     ('Sarah Connor', 'sarah@skynet.com', '123456'), 
     ('T-101', 't101@skynet.com', '123456')
 ;
+
+SELECT * FROM tb_usuarios WHERE id = 1;
+SELECT * FROM tb_usuarios ORDER BY nome DESC LIMIT 1;
