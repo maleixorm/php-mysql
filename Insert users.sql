3712c4f977f2c5ed09639c4647d00698215226ae
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-28 11:16:34 -0300

    Inserindo dados na tabela users

---
+++ Insert users.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM fullstack.users
+LIMIT 0, 1000
+
+-- Date: 2025-07-02 14:11
+*/
+INSERT INTO `users` (`id`,`name`,`lastname`,`email`,`password`) VALUES (1,'marcos','marques','contato@marcosaleixo.com.br','1234');
+INSERT INTO `users` (`id`,`name`,`lastname`,`email`,`password`) VALUES (2,'andre','marques','sql@marcosaleixo.com.br','1234');
+INSERT INTO `users` (`id`,`name`,`lastname`,`email`,`password`) VALUES (3,'sarah','marques','arte@marcosaleixo.com.br','1234');
+INSERT INTO `users` (`id`,`name`,`lastname`,`email`,`password`) VALUES (4,'helena','marques','kids@marcosaleixo.com.br','1234');
+INSERT INTO `users` (`id`,`name`,`lastname`,`email`,`password`) VALUES (5,'john','reese','john@skynet.com','1234');
