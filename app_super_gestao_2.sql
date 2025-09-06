716d4a1dfd61d8190e2e5c4538c0fc86994b9fbc
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-06 20:38:46 -0300

    Consultas das tabelas do app super gestão do laravel

---
+++ app_super_gestao_2.sql
@@ -0,0 +1,11 @@
+use sg;
+
+select * from migrations;
+select * from fornecedores;
+select * from site_contatos;
+
+describe produtos;
+describe produto_detalhes;
+describe fornecedores;
+
+select * from tweets;
